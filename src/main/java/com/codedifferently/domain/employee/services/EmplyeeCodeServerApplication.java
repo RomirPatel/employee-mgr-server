795b8c624420d54efdde347e2c8115ba79cd8f97
Romir Patel <romirpatel308@gmail.com>
2023-04-19 16:30:48 -0400

    Finished crud api

---
+++ src/main/java/com/codedifferently/domain/employee/services/EmplyeeCodeServerApplication.java
@@ -0,0 +1,13 @@
+package com.codedifferently.domain.employee.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class EmployeeCodeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeCodeServerApplication.class, args);
+    }
+
+}
